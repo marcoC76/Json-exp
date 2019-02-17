17b7c0a28a0670756477470ae1dbb1ac31cdf321
Marco Antonio <marco.perez.cetis76@gmail.com>
2019-02-16 19:29:58 -0600

    mas

--- busca.js
+++ busca.js
@@ -21,22 +21,23 @@ function recibir() {
     var newArray = obj.filter(function (el) {
         return (el.iD === valor);
     });
-    
+
     salida = `
-            <center> <h2>${newArray[0].completo}</h2></center>
+            <center> <h2>${newArray[0].nombre}</h2></center>
+            <center> <h5>${newArray[0].aPaterno} ${newArray[0].aMaterno}</h5></center>
             <hr>
             <div class="row">
                 <div class="col s12 m3 center">
-                    <h4>Grupo: ${newArray[0].grupo}</h4>
+                    <h4>Grupo: <a class="waves-effect waves-light btn-large btn-floating teal darken-4" style="font-size:90%;">${newArray[0].grupo}</a></h4>
                 </div>
                 <div class="col s12 m3 center">
-                    <h5>Equipo:<br> ${newArray[0].equipo}</h5>
+                    <h5 >Equipo:</h5> <a class="waves-effect btn waves-light white black-text" style="font-size:100%;">${newArray[0].equipo}</a> </h5>
                 </div>
                 <div class="col s12 m3 center">
-                    <h4>Asistencias: <a class="waves-effect waves-light btn-floating red" style="font-size:90%;"> ${newArray[0].tOTALASIS}</a></h4>
+                    <h4>Asistencias: <a class="waves-effect waves-light btn-large btn-floating red" style="font-size:90%;"> ${newArray[0].tOTALASIS}</a></h4>
                 </div>
                 <div class="col s12 m3 center">
-                    <h4>Punto Extra: <a class="waves-effect waves-light btn-floating green" style="font-size:90%;"> ${newArray[0].pUNTOEX}</a></h4> 
+                    <h4>Punto Extra: <a class="waves-effect waves-light btn-large btn-floating green" style="font-size:90%;"> ${newArray[0].pUNTOEX}</a></h4> 
                     
                 </div>
             </div>
@@ -49,18 +50,20 @@ function recibir() {
             <div class="row">
                 <div class="col s12 m6">
                     <div class="card grey lighten-2">
-                        <div class="card-content ">
+                        <div class="card-content center">
                         <span class="card-title center">Actividades</span>
-                            <h5>Puntos totales: <a class="waves-effect waves-light btn blue" style="font-size:90%;">${newArray[0].pUNTOSACT}</a> </h5>
+                            <h5>Puntos totales: <a class="waves-effect waves-light btn blue" style="font-size:90%;">${newArray[0].pUNTOSACT}</a>  
+                            Promedio: <a class="waves-effect waves-light btn green" style="font-size:90%;">${newArray[0].pROMACT}</a></h5>
                             <canvas id="myChart" width="50"></canvas>
                         </div>
                     </div>
                 </div>
                 <div class="col s12 m6">
                     <div class="card  teal lighten-4">
-                        <div class="card-content ">
+                        <div class="card-content center">
                         <span class="card-title center">Cuestionarios</span>
-                            <h5>Puntos totales: <a class="waves-effect waves-light btn blue" style="font-size:90%;"> ${newArray[0].pUNTOSCUES}</a></h5>
+                            <h5>Puntos totales: <a class="waves-effect waves-light btn blue" style="font-size:90%;"> ${newArray[0].pUNTOSCUES}</a> 
+                            Promedio: <a class="waves-effect waves-light btn green " style="font-size:90%;">${newArray[0].pROMCUES}</a></h5>
                             <canvas id="myChart2" width="50"></canvas>
                         </div>
                     </div>
@@ -69,7 +72,7 @@ function recibir() {
             <div class="row">
                 <div class="col s12 m6">
                     <div class="card red lighten-4">
-                        <div class="card-content ">
+                        <div class="card-content center">
                         <span class="card-title center">Proyecto</span>
                             <h5>Puntos totales: <a class="waves-effect waves-light btn blue" style="font-size:90%;">${newArray[0].pUNTOSPRO}</a></h5>
                             <canvas id="myChart3" width="50"></canvas>
@@ -78,7 +81,7 @@ function recibir() {
                 </div>
                 <div class="col s12 m6">
                     <div class="card blue lighten-4">
-                        <div class="card-content ">
+                        <div class="card-content center">
                         <span class="card-title center">Bitacora</span>
                             <h5>Puntos totales: <a class="waves-effect waves-light btn blue" style="font-size:90%;">${newArray[0].pUNTOSBIT}</a></h5>
                             <canvas id="myChart4" width="50"></canvas>
@@ -90,27 +93,28 @@ function recibir() {
         
     `;
     document.getElementById("resultado").innerHTML = salida;
-    var estado="";
-    if(newArray[0].fINAL<6){
-        estado="REPROBADO";
-    }else{
-        estado="APROBADO";
+    var estado = "";
+    if (newArray[0].fINAL < 6) {
+        estado = "REPROBADO";
+    } else {
+        estado = "APROBADO";
     }
     document.getElementById("estado").innerHTML = estado;
-    
+
     var ctx = document.getElementById("myChart");
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ["Act1", "Act2", "Act3", "Act4", "Act5"],
+            labels: ["Act1", "Act2", "Act3", "Act4", "Act5", "ActR"],
             datasets: [{
                 label: 'Calificación',
-                data: [newArray[0].aCTIVIDAD1, newArray[0].aCTIVIDAD2,newArray[0].aCTIVIDAD3,newArray[0].aCTIVIDAD4,newArray[0].aCTIVIDAD5],
+                data: [newArray[0].aCTIVIDAD1, newArray[0].aCTIVIDAD2, newArray[0].aCTIVIDAD3, newArray[0].aCTIVIDAD4, newArray[0].aCTIVIDAD5, newArray[0].aCTIVIDADR],
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.6)',
                     'rgba(255, 206, 86, 0.6)',
                     'rgba(75, 192, 192, 0.6)',
                     'rgba(255, 99, 132, 0.6)',
+                    'rgba(200, 100, 150, 0.6)',
                     'rgba(153, 102, 255, 0.6)'
                 ]
             }]
@@ -119,7 +123,7 @@ function recibir() {
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero:true
+                        beginAtZero: true
                     }
                 }]
             }
@@ -132,7 +136,7 @@ function recibir() {
             labels: ["Cuest1", "Cuest2", "Cuest3", "Cuest4", "Cuest5"],
             datasets: [{
                 label: 'Calificación',
-                data: [newArray[0].cUESTIONARIO1, newArray[0].cUESTIONARIO2,newArray[0].cUESTIONARIO3,newArray[0].cUESTIONARIO4,newArray[0].cUESTIONARIO5],
+                data: [newArray[0].cUESTIONARIO1, newArray[0].cUESTIONARIO2, newArray[0].cUESTIONARIO3, newArray[0].cUESTIONARIO4, newArray[0].cUESTIONARIO5],
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.6)',
                     'rgba(255, 206, 86, 0.6)',
@@ -146,7 +150,7 @@ function recibir() {
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero:true
+                        beginAtZero: true
                     }
                 }]
             }
@@ -154,50 +158,33 @@ function recibir() {
     });
     var ctx = document.getElementById("myChart3");
     var myChart3 = new Chart(ctx, {
-        type: 'bar',
+        type: 'pie',
         data: {
-            labels: ["Proyecto"],
+            labels: ["Proyecto", ""],
             datasets: [{
                 label: 'Calificación',
-                data: [newArray[0].pROYECTO],
+                data: [newArray[0].pROYECTO, 10 - newArray[0].pROYECTO],
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.6)'
-                    
+
                 ]
             }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
         }
     });
     var ctx = document.getElementById("myChart4");
     var myChart4 = new Chart(ctx, {
-        type: 'bar',
+        type: 'pie',
         data: {
-            labels: ["Bitácora"],
+            labels: ["Bitácora", ""],
             datasets: [{
                 label: 'Calificación',
-                data: [newArray[0].bITACORA],
+                data: [newArray[0].bITACORA, 10 - newArray[0].bITACORA],
                 backgroundColor: [
                     'rgba(153, 102, 255, 0.6)'
                 ]
             }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
         }
+
     });
 }
 
