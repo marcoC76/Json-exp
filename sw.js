a5f8e5cb98e832fc7243c713959d64aa7f9c9324
Marco Antonio <marco.perez.cetis76@gmail.com>
2019-02-22 13:30:59 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,25 +16,56 @@ var urlsCache = [
     './favicon-16x16.png',
     './fancy-pants.jpg',
     './js/init.js',
-    './js/materialize.js',
+    './js/materialize.js'
 ];
 
 //instala y guarda los recursos
-self.addEventListener('install', e =>{
+self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE_NAME)
-                .then(cache =>{
-                    return cache.addAll(urlsCache)
-                                .then(()=>{
-                                    self.skipWaiting();
-                                })
-                                .catch(err=>{
-                                    console.log('no se pudo cachear');
-                                })
-                })
+            .then(cache => {
+                return cache.addAll(urlsCache)
+                    .then(() => {
+                        self.skipWaiting();
+                    })
+            })
+            .catch(err => {
+                console.log('no se pudo cachear');
+            })
     )
 });
 
 //activar
 
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME];
+    e.waitUntil(
+        caches.keys()
+            .then(cachesNames => {
+                return Promise.all(
+                    cachesNames.map(cachesName => {
+                        if (cacheWhitelist.indexOf(cachesName) === -1) {
+                            //borar
+                            return caches.delete(cachesName);
+                        }
+                    })
+                );
+            })
+            .then(() => {
+                self.clients.claim();
+            })
+    );
+});
+
 //fetch
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request)
+            .then(res => {
+                if (res) {
+                    return res;
+                }
+                return fetch(e.request);
+            })
+    );
+});
